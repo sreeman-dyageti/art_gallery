7ff8733f481f012431419f4c3fb64d5c3890de62
sreeman-dyageti <dyagetisaisreeman@example.com>
2026-02-08 22:33:24 +0530

    feat: configure Express middleware and view engine

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
 
 app.get("/", (req, res) => {
   res.render("/public/index.html");
