27f75a27b78b4937238a4d27eb5b6b0459cc5147
sreeman-dyageti <dyagetisaisreeman@example.com>
2026-02-10 18:08:30 +0530

    feat:Add New Post Option

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const posts=[];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,9 +14,24 @@ app.set("view engine", "ejs");
 
 //home page ,All posts
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs",{posts});
 });
 
+
+// show create post page 
+app.get("/new",(req,res)=>{
+  res.render("new");
+});
+
+//create post 
+app.post("/create",(req,res)=>{
+const{text,content}=req.body;
+
+posts.push({
+  text:text,
+  content:content
+  });
+});
 
 
 app.listen(port, () => {
